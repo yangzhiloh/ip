0cfe9ff3a1c5026158541d81b0ccb230fc794274
Loh Yang Zhi <yangzhiloh@gmail.com>
2026-08-19 03:16:28 +0800

    Rename chatbot to Esther and add initial greeting

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Intermediate with basic Java knowledge
+* IDE and level of expertise: Intermediate using Visual Studio Code
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Esther.java
@@ -0,0 +1,12 @@
+public class Esther {
+    public static void main(String[] args) {
+        String line = "___________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Heyyyy! I'm your favourite assistant Esther.");
+        System.out.println("What can I do for you today?");
+        System.out.println(line);
+        System.out.println("Byeee! Hope to see you soooon!");
+        System.out.println(line);
+    }
+}
