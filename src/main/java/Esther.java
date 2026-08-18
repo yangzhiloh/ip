77c2d5ad70240cbb37d36e163e4ab96c3afa77c1
Loh Yang Zhi <yangzhiloh@gmail.com>
2026-08-19 03:24:01 +0800

    Add command echoing and bye handling

--- src/main/java/Esther.java
+++ src/main/java/Esther.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Esther {
     public static void main(String[] args) {
         String line = "___________________________________________________________";
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(line);
         System.out.println("Heyyyy! I'm your favourite assistant Esther.");
         System.out.println("What can I do for you today?");
         System.out.println(line);
-        System.out.println("Byeee! Hope to see you soooon!");
-        System.out.println(line);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Byeee! Hope to see you soooon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(command);
+            System.out.println(line);
+        }
+
+        scanner.close();
+        
     }
 }
