b8c321edb41e2a9a85b71b33a6bd1b21bd6d4b56
Loh Yang Zhi <yangzhiloh@gmail.com>
2026-08-19 23:37:05 +0800

    Introduce Task class for task status management

--- src/main/java/Esther.java
+++ src/main/java/Esther.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
+
 public class Esther {
     public static void main(String[] args) {
         String line = "___________________________________________________________";
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println(line);
@@ -27,49 +27,65 @@ public class Esther {
                 int tasksLeft = 0;
 
                 for (int i = 0; i < taskCount; i++) {
-                    if (!isDone[i]) {
+                    if (!tasks[i].isDone()) {
                         tasksLeft++;
                     }
                 }
                 System.out.println(line);
 
-                if (tasksLeft == 0) {System.out.println("WOW! You have no tasks left, good work!");}
+                if (tasksLeft == 0) {System.out.println(
+                    "WOW! You have no tasks left, good work!"
+                );}
 
                 if (1 <= tasksLeft && tasksLeft <= 5) {
                     String taskWord = tasksLeft == 1 ? "task" : "tasks";
-                    System.out.println(String.format("Only %d %s left! Should be a piece of cake for you!", tasksLeft, taskWord));
+                    System.out.println(String.format(
+                        "Only %d %s left! Should be a piece of cake for you!",
+                        tasksLeft,
+                        taskWord
+                    ));
                 }
 
-                if (tasksLeft > 5) {System.out.println(String.format("%d tasks left?? What have you been doing this whole time?? You better focus up!", tasksLeft));}
+                if (tasksLeft > 5) {
+                    System.out.println(String.format(
+                    "%d tasks left?? What have you been doing this whole time?? You better focus up!",
+                    tasksLeft
+                    ));
+                }
 
                 for (int i = 0; i < taskCount; i++) {
-                    String statusIcon = isDone[i] ? "X" : " ";
-                    System.out.println(String.format("%d.[%s] %s", i + 1, statusIcon, tasks[i]));
+                    System.out.println(String.format(
+                        "%d.%s",
+                        i + 1,
+                        tasks[i].toString()
+                    ));
                 }
 
                 System.out.println(line);
             } else if (command.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(command.substring(5).trim());
                 int taskIndex = taskNumber - 1;
+                Task task = tasks[taskIndex];
 
-                isDone[taskIndex] = true;
+                task.markAsDone();
 
                 System.out.println(line);
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[taskIndex]);
+                System.out.println(" " + task);
                 System.out.println(line);
             } else if (command.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(command.substring(7).trim());
                 int taskIndex = taskNumber - 1;
+                Task task = tasks[taskIndex];
 
-                isDone[taskIndex] = false;
+                task.markAsNotDone();
 
                 System.out.println(line);
                 System.out.println("Okay! I've marked this task as not done.");
-                System.out.println(" [ ] " + tasks[taskIndex]);
+                System.out.println(" " + task);
                 System.out.println(line);
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 String taskWord = taskCount == 1 ? "task" : "tasks";
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+/**
+ * Represents a task tracked by Esther.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "[%s] %s",
+            getStatusIcon(),
+            this.description
+        );
+    }
+}
